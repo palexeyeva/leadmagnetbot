c0e091f4461c70f449c2421ce31ab9239173a6b6
Polina Nechaeva <polinaa12@gmail.com>
2025-07-04 11:54:35 +0300

    Включен WAL-режим и сденан движок SQLite протоколобезопасным

--- healthcheck.py
+++ healthcheck.py
@@ -1,16 +1,18 @@
+# healthcheck.py
 from threading import Thread
 from aiohttp import web
+
 
 async def handle(request):
     return web.Response(text="OK")
 
+
 def start_healthcheck_server():
-    """
-    Запускает aiohttp-сервер на 0.0.0.0:8000 в отдельном потоке.
-    """
     app = web.Application()
     app.add_routes([web.get("/", handle)])
-    web.run_app(app, host="0.0.0.0", port=8000)
+
+    web.run_app(app, host="0.0.0.0", port=8000, handle_signals=False)
+
 
 def run():
     t = Thread(target=start_healthcheck_server, daemon=True)
