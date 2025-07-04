21c4a6d3d5f0a6342fbf11ab4e7ba8c873c83fe5
Polina Nechaeva <polinaa12@gmail.com>
2025-07-05 00:10:08 +0300

    healthcheack port

--- healthcheck.py
+++ healthcheck.py
@@ -1,6 +1,7 @@
 # healthcheck.py
 from threading import Thread
 from aiohttp import web
+import os
 
 
 async def handle(request):
@@ -11,7 +12,8 @@ def start_healthcheck_server():
     app = web.Application()
     app.add_routes([web.get("/", handle)])
 
-    web.run_app(app, host="0.0.0.0", port=8000, handle_signals=False)
+    port = int(os.getenv("PORT", "8000"))
+    web.run_app(app, host="0.0.0.0", port=port, handle_signals=False)
 
 
 def run():
